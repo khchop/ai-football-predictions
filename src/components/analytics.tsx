3b34b1acce60edc0502663bca368bab535d44679
Pieter Bos <pieterbos1987@gmail.com>
2026-01-25 21:08:44 +0100

    fix: update Umami tracking script to HTTPS and new domain

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script';
 export function Analytics() {
   return (
     <Script
-      src="http://umami-w8swgckkgosssco00k0go4g8.188.245.241.135.sslip.io/script.js"
+      src="https://umami.kroam.xyz/script.js"
       data-website-id="2e966abd-99fc-4363-8007-3737d99bc4c1"
       strategy="afterInteractive"
     />
